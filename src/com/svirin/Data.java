4752966715d781043513296572cf29b6810bee76
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2019-11-25 20:24:35 +0200

    Update Data.java

--- src/com/svirin/Data.java
+++ src/com/svirin/Data.java
@@ -1,5 +1,7 @@
 package com.svirin;
 
+import com.svirin.controller.Controller;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -13,27 +15,28 @@ import java.util.List;
 public class Data {
     List<String> finalstr = new ArrayList<String>();
     List<String> lines = new ArrayList<String>();
-    List<Boolean> state = new ArrayList<Boolean>();
-    List<String> names = new ArrayList<>();
-    List<String> hashes = new ArrayList<>();
+    public static List<Boolean> state = new ArrayList<Boolean>();
+    public static List<String> names = new ArrayList<>();
+    public static List<String> hashes = new ArrayList<>();
     String rootfile = new String();
     String md5 = new String();
     File temp;
 
     public Data(){}
     Data(File dir,String dir1,String dir2) {// input data from explorer
+        Controller.key = true;
          temp = new File(String.valueOf(dir));
          md5 = dir1;
          rootfile = dir2;
     }
 
     void transform() throws IOException, NoSuchAlgorithmException { //create line like *......
-        Main main = new Main();
         String line;
         BufferedReader reader = new BufferedReader(new FileReader(md5));
-        File[] array = temp.listFiles();
-        String files[] = Arrays.stream(array).map(File::getAbsolutePath)
-                .toArray(String[]::new);
+        //adding ONLY files, without directories
+        List<String> files = new ArrayList<>();
+        for(File x : temp.listFiles())
+            if(!x.isDirectory()) files.add(x.getAbsolutePath());
         rootfile += "\\";
         int i = 0;
         for(String a : files){
@@ -44,12 +47,12 @@ public class Data {
 
          i = 0;
         for (String a : files) {
-            a = main.md5(a);
-            files[i] = a;
+            a = Main.md5(a);
+            files.set(i, a);
             i++;
         }
-        for (i = 0;i<files.length;i++) {
-            finalstr.set(i,files[i] + finalstr.get(i));
+        for (i = 0;i<files.size();i++) {
+            finalstr.set(i,files.get(i) + finalstr.get(i));//string like "<hash> *filename"
         }
 
         while ((line = reader.readLine()) != null) {
@@ -60,19 +63,20 @@ public class Data {
         //check for equality
         int z = 0;
         for( i=1;i<lines.size();i++){
-            state.add(false);
             for(int k=0;k<finalstr.size();k++){
                 if(lines.get(i).equals(finalstr.get(k))) {
-                    state.set(i - 1, true);
+                    state.add(true);
                     names.add(lines.get(i).substring(33));
                     hashes.add(finalstr.get(k).replace(names.get(z),""));
                     System.out.println(hashes.get(z));
                     z++;
                 }
             }
+            if(!names.contains(lines.get(i).substring(33))){
+                state.add(false);
+                names.add(lines.get(i).substring(33));
+                hashes.add("Checksum did not match.");
+            }
         }
-        System.out.println(state);
-
-
     }
 }
