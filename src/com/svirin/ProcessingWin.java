2dfc39267da29d8c9ba97a4faa076b5bdfacbd87
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2019-12-04 20:21:41 +0200

    Update ProcessingWin.java

--- src/com/svirin/ProcessingWin.java
+++ src/com/svirin/ProcessingWin.java
@@ -1,5 +1,5 @@
-package com.svirin;
-
+package com.svirin;//Ім'я пакету (бібліотеки), до якого відноситься клас
+//Підключення потрібних пакетів (бібліотек)
 import com.svirin.POJO.FilePOJO;
 import com.svirin.States;
 import javafx.collections.FXCollections;
@@ -15,21 +15,30 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.File;
-
+//Клас, що загружає файл розмітки вікна та відкриває це вікно
 public class ProcessingWin {
+    //Вікно з результатами роботи програми
     private Stage processStage;
+    //Контейнер, що зберігатиме всі елементи інтерфейсу
     private AnchorPane ap;
 
 
-
+    //Конструктор класу, що загружає файл розмітки .fxml. В конструкторі може
+    //бути виключення - програма може не знайти файл розмітки, тож ключові слова
+    //throws Exception попереджують про те, що при створенні класу слід
+    //перехопити та обробити виключення.
     public ProcessingWin() throws Exception{
+        //Завантажити файл розмітки можна за допомогою методу load класу FXMLLoader
+        //Відкриваємо файл методом getResource
         ap = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
+        //Створюємо постанову (об'єкт класу Scene), передаючи в аргумент контейнер з
+        //усіма елементами інтерфейсу
         Scene sc = new Scene(ap);
+        //Створюємо сцену, на яку ставимо постанову методом setScene
         processStage = new Stage();
         processStage.setScene(sc);
-        //processStage.setResizable(true);
     }
-
+    //Метод, що повертає вікно з результатами роботи програми
     public Stage getProcessStage(){
         return processStage;
     }
