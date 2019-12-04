80afb6640b7b7a96ed36790af51335e88d6f343c
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2019-12-05 01:23:13 +0200

    Update FilePOJO.java

--- src/com/svirin/POJO/FilePOJO.java
+++ src/com/svirin/POJO/FilePOJO.java
@@ -2,18 +2,16 @@ package com.svirin.POJO;
 
 import com.svirin.States;
 import javafx.scene.shape.Circle;
-
+//Клас, поля якого відображатимуться в таблиці
 public class FilePOJO {
-    private Circle state = States.UNPROCESSED.getState();
-    private String name;
-    private String hash = "";
-
+    private Circle state = States.UNPROCESSED.getState(); //Стан файла
+    private String name; //Ім'я файла
+    private String hash = ""; //Хеш-сума файла
+    //Конструктор, що приймає ім'я файла в якості аргумента
     public FilePOJO(String nameArg){
         name = nameArg;
     }
-
-    public FilePOJO(){}
-
+    //Методи для встановлення нового та отримання поточного значення private членів
     public Circle getState(){
         return state;
     }
