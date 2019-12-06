1d3565c77ce3f2e4f676669ca53f5ac17df3a37d
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2019-12-06 22:28:52 +0200

    Update ProcessingWin.java

--- src/com/svirin/ProcessingWin.java
+++ src/com/svirin/ProcessingWin.java
@@ -1,17 +1,8 @@
 package com.svirin;//Ім'я пакету (бібліотеки), до якого відноситься клас
 //Підключення потрібних пакетів (бібліотек)
-import com.svirin.POJO.FilePOJO;
-import com.svirin.States;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.File;
