5a84f8ee0f0484065b8233ca55b4f1201bab4f7b
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2019-12-08 15:31:19 +0200

    Update ProcessingWin.java

--- src/com/svirin/ProcessingWin.java
+++ src/com/svirin/ProcessingWin.java
@@ -3,9 +3,7 @@ package com.svirin;//Ім'я пакету (бібліотеки), до яког�
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import java.io.File;
 //Клас, що загружає файл розмітки вікна та відкриває це вікно
 public class ProcessingWin {
     //Вікно з результатами роботи програми
